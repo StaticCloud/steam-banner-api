3bf44ea699da235ff9ec06e24bd804c7276be12c
StaticCloud <diegomarrs@gmail.com>
2025-07-29 19:42:25 -0700

    implemented multithreading solution for obtaining ids of completed games

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ app = FastAPI()
 
 client = SteamAPI()
 
-@app.get("/{user_id}")
-def get_user_achievements(user_id: str):
-    response = client.get_completed_game_data(user_id)
+@app.get("/games/{user_id}")
+def get_owned_game_ids(user_id: str):
+    response = client.get_owned_game_ids(user_id)
+
     return response
 
+@app.get("/games/{user_id}/completed")
+def get_completed_game_ids(user_id: str):
+    response = client.get_completed_game_ids(user_id)
+
+    return response
--- steamapi/__init__.py
+++ steamapi/__init__.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from dotenv import load_dotenv
 import requests
 import os
@@ -11,31 +12,42 @@ class SteamAPI():
     def get_owned_game_ids(self, steam_id: str):
         try:
             url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={self.api_key}&steamid={steam_id}&format=json'
-            res = requests.get(url).json()
+            res = requests.get(url, 5).json()
 
             game_ids = [game['appid'] for game in res['response']['games']]
 
             return game_ids
-        except OSError:
-            return OSError
+        except Exception as e:
+            return []
     
-    def get_completed_game_data(self, steam_id: str):
-        try:
-            game_ids = self.get_owned_game_ids(steam_id=steam_id)
+    def get_completed_game_ids(self, steam_id: str):
+        game_ids = self.get_owned_game_ids(steam_id)
 
-            completed_game_ids = []
+        completed_game_ids = []
 
-            # Not practical. The Steam Web API has forced my hand.
-            for id in game_ids:
-                url = f'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={id}&key={self.api_key}&steamid={steam_id}'
-                res = requests.get(url).json()
+        def check_completion(game_id):
+            try:
+                url = f'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={game_id}&key={self.api_key}&steamid={steam_id}'
+                res = requests.get(url, 5).json()
 
-                if 'achievements' in res['playerstats']:
-                    self.validate_completion_status(res['playerstats']['achievements']) and completed_game_ids.append(id)
+                achievements = res['playerstats']['achievements']
+
+                if achievements and self.validate_completion_status(achievements):
+                    return game_id
+            except Exception as e:
+                pass
+            return None
+        
+        with ThreadPoolExecutor(10) as executor:
+            futures = [executor.submit(check_completion, game_id) for game_id in game_ids]
+
+            for future in as_completed(futures):
+                result = future.result()
+
+                if result:
+                    completed_game_ids.append(result)
             
-            return completed_game_ids
-        except OSError:
-            return OSError
+        return completed_game_ids
     
     def validate_completion_status(self, achievements: list):
         return all(achievement['achieved'] == 1 for achievement in achievements)
