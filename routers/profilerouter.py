cbf95e2bab14b7411a80e499b827ac9b76fc8ad8
StaticCloud <diegomarrs@gmail.com>
2025-07-31 14:07:29 -0700

    reworked banner filtering to be a query parameter instead of an endpoint

--- routers/profilerouter.py
+++ routers/profilerouter.py
@@ -8,14 +8,14 @@ profile_router = APIRouter()
 
 banner_helper = BannerHelper()
 
-@profile_router.get("/{banner_type}/{user_id}")
-def get_header_by_user_id(banner_type: str, user_id: str, completed: str):
+@profile_router.get("/{user_id}")
+def get_header_by_user_id(bannertype: str, user_id: str, completed: str):
     response = {}
 
     response['profile'] = client.get_player_profile(user_id)
 
     game_ids = client.get_owned_game_ids(user_id) if completed == "false" else client.get_completed_game_ids(user_id) if completed == "true" else None
 
-    response['banners'] = banner_helper.get_box_art_urls(game_ids) if banner_type == "box-art" else banner_helper.get_header_urls(game_ids) if banner_type == "header" else banner_helper.get_box_art_urls(game_ids)
+    response['banners'] = banner_helper.get_box_art_urls(game_ids) if bannertype == "boxart" else banner_helper.get_header_urls(game_ids) if bannertype == "header" else banner_helper.get_box_art_urls(game_ids)
 
     return response
