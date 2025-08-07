d428f0d4a32ad45c97a2def8f896ea87ef67ef16
StaticCloud <diegomarrs@gmail.com>
2025-08-07 12:00:58 -0700

    backtracking on decision to include hx-requests

--- routers/gamerouter.py
+++ routers/gamerouter.py
@@ -1,6 +1,4 @@
-from core.templates import templates
-from fastapi import APIRouter, HTTPException, Query, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import APIRouter, HTTPException, Query
 from steamapi import SteamAPI
 from utils import BannerHelper
 from pydantic import BaseModel
@@ -14,9 +12,8 @@ banner_helper = BannerHelper()
 class Body(BaseModel):
     game_ids: list[int]
 
-@game_router.get("/", response_class=HTMLResponse)
+@game_router.get("/")
 async def get_header_by_game_id(
-        request: Request,
         body: Body, 
         bannertype: str = Query(..., regex="^(boxart|header)$"),
     ):
@@ -26,11 +23,8 @@ async def get_header_by_game_id(
             banners = banner_helper.get_header_urls(body.game_ids)
         else:
             banners = banner_helper.get_box_art_urls(body.game_ids)
-        
-        if request.headers.get("hx-request") == "true":
-            return templates.TemplateResponse("partials/_game_banners.html", { "request": request, "games": body.game_ids })
-    
+         
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error fetching banners: {str(e)}")
     
-    return JSONResponse(banners)
+    return banners
